fdbeb0893250f8d5895919be7be6102f4f4463ef
siwiec987 <siwiec987@gmail.com>
2026-04-07 02:43:23 +0200

    localizedError, but not localized yet

--- FlashcardQuizEdu/Models/ExtractError.swift
+++ FlashcardQuizEdu/Models/ExtractError.swift
@@ -8,7 +8,25 @@
 
 import Foundation
 
-enum ExtractError: Error {
+enum ExtractError: LocalizedError {
     case fileUnreadable
     case fileEmpty
+
+    var errorDescription: String? {
+        switch self {
+        case .fileUnreadable:
+            "Nie udało się odczytać zawartości pliku."
+        case .fileEmpty:
+            "Nie znaleziono tekstu do wyodrębnienia."
+        }
+    }
+
+    var recoverySuggestion: String? {
+        switch self {
+        case .fileUnreadable:
+            "Upewnij się, że plik nie jest uszkodzony i spróbuj ponownie."
+        case .fileEmpty:
+            "Wybierz plik lub zdjęcie zawierające czytelny tekst."
+        }
+    }
 }
